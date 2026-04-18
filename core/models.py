bc84d6bd4471a5a8f444fe5d051d376012c5c9db
NandhuT17 <nandhakishor261@gmail.com>
2026-04-19 00:22:41 +0530

    create class in models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Job(models.Model) :
+    job_title = models.CharField()
+    job_description = models.TextField()
+    required_skills = models.CharField()
+    required_experience = models.IntegerField()
+    created_by = models.ForeignKey(User,on_delete=models.CASCADE)
+    created_time = models.TimeField(auto_now_add = True)
+
+    def __str__(self) :
+        return self.job_title
