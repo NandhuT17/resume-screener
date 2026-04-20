accd597fb4895e8ede9ddde19e989a724d0a1428
NandhuT17 <nandhakishor261@gmail.com>
2026-04-20 18:56:24 +0530

    create profile and job models

--- core/models.py
+++ core/models.py
@@ -3,13 +3,26 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Job(models.Model) :
-    job_title = models.CharField()
-    job_description = models.TextField()
-    required_skills = models.CharField()
-    required_experience = models.IntegerField()
-    created_by = models.ForeignKey(User,on_delete=models.CASCADE)
-    created_time = models.TimeField(auto_now_add = True)
+class Profile(models.Model) :
+    role_choices = (
+        ('recruiter','Recruiter'),
+        ('canditate','Canditate')
+    )
+
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    role = models.CharField(max_length=20,choices=role_choices)
 
     def __str__(self) :
-        return self.job_title
+        return f"{self.user.username} - {self.role}"
+    
+
+class Job(models.Model) :
+    recruiter = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100,null = True,blank = True)
+    description = models.TextField()
+    location = models.CharField(max_length=100,null = True, blank=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self) :
+        return self.title
+    
