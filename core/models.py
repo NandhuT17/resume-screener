c5075d5e75cc420169367ffc06751041fdce70d6
NandhuT17 <nandhakishor261@gmail.com>
2026-04-20 18:59:37 +0530

    create resume and application models

--- core/models.py
+++ core/models.py
@@ -26,3 +26,31 @@ class Job(models.Model) :
     def __str__(self) :
         return self.title
     
+
+class Resume(models.Model) :
+    candidate = models.ForeignKey(User,on_delete = models.CASCADE)
+    file = models.FileField(upload_to='resumes/')
+    uploaded_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self) :
+        return f"{self.candidate.username} - Resume"
+
+
+
+class Application(models.Model) :
+    status = (
+        ('applied', 'Applied'),
+        ('reviewed', 'Reviewed'),
+        ('selected', 'Selected'),
+        ('rejected', 'Rejected')
+    )
+
+    candidate = models.ForeignKey(User,on_delete = models.CASCADE)
+    job = models.ForeignKey(Job, on_delete = models.CASCADE)
+    resume = models.ForeignKey(Resume, on_delete = models.CASCADE )
+    score = models.FloatField(default = 0)
+    status = models.CharField(max_length=20,choices=status,default='applied')
+    applied_at = models.DateTimeField(auto_now_add = True) 
+
+    def __str__(self) :
+        return f"{self.candidate.username} - {self.job.title}"
