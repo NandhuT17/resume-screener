8b0f4efb1ced46f31bb2b821c5735b06b1818e33
NandhuT17 <nandhakishor261@gmail.com>
2026-04-21 15:59:03 +0530

    create login page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import *
+from .forms import *
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm
 
-def index(request) :
-    return HttpResponse('Hello world')
+
+# Create your views here.
+def login_view(request):
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('core/recruiter_dashboard')
+            else:
+                context = {
+                    'form' : form,
+                }
+                return render(request, 'core/login.html', context)
+    context = {
+        'form' : form,
+    }
+    return render(request, 'core/login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
